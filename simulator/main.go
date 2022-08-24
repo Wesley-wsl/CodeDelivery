feed872230793f29080d77859c76a7e4beda665c
Wesley Westelley <wesleywestelley@gmail.com>
2022-08-23 23:39:33 -0300

    :white_check_mark: feat: start to consumer and produce routes

--- simulator/main.go
+++ simulator/main.go
@@ -1,38 +1,29 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"log"
 
-	// route2 "github.com/codeedu/imersaofsfc2-simulator/application/route"
+	kafka2 "github.com/codeedu/imersaofsfc2-simulator/application/kafka"
 	"github.com/codeedu/imersaofsfc2-simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/joho/godotenv"
 )
 
 func init() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file.")
+		log.Fatal("error loading .env file")
 	}
 }
 
 func main() {
-
-	producer := kafka.NewKafkaProducer()
-
-	kafka.Publish("Hello World!", "readtest", producer)
-
-	for {
-		_ = 1
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+	for msg := range msgChan {
+		fmt.Println("Message....")
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
 	}
-
-	// route := route2.Route{
-	// 	ID:       "1",
-	// 	ClientID: "1",
-	// }
-
-	// route.LoadPositions()
-	// stringjson, _ := route.ExportJsonPositions()
-
-	// fmt.Println(stringjson[0])
 }
